59f7776f3b22bd54de42f77fc7192100f0112b09
TPlemur <TPlemur@gmail.com>
2022-04-14 23:35:36 -0700

    added hints

--- src/confuse.js
+++ src/confuse.js
@@ -6,3 +6,18 @@ function encrypt (inString){
     for(i=0;i<inString.length -1;i+=2){outString=outString.substring(0,i)+outString[i+1]+outString[i]+outString.substring(i+2);}
     return outString;
 }
+
+//returns a random hint from the list
+function randHint(){
+    var hints = [
+        "I'm sure you'll get around to the answer eventually.",
+        "I hope this puzzle doesn't have you bordering on madness.",
+        "If androids dream of electric sheep, \ncould one count to 26 before falling asleep?",
+        "I could really go for some Bacon right now.",
+        "If you're desparate you could always try inspect element.",
+        "Don't feel left behind, I'm sure you're heading right on track.",
+        "If you feel like your going round in circles, \ndon't worry, you are closer than you think",
+        "Zeros stay where they are, don't be a zero, \nget out there and do something."
+    ];
+    return hints[Math.floor(Math.random()*hints.length)];
+}
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -7,19 +7,22 @@ class Menu extends Phaser.Scene {
         this.load.html("form","src/form.html")
     }
     create(){
+        //all the text
         this.titleText = this.add.text(screenWidth/2, screenHeight/2 - 410, 'Welcome', menuConfig).setOrigin(0.5);
         this.descriptionText = this.add.text(screenWidth/2, screenHeight/2 - 310, 'Please input the code below', textConfig).setOrigin(0.5);
         this.resultText = this.add.text(screenWidth/2, screenHeight/2 - 110, encrypt(""), textConfig).setOrigin(0.5);
+        this.hintText = this.add.text(screenWidth/2, screenHeight/2 - 10, "", textConfig).setOrigin(0.5);
         
-
+        //input box
         this.nameInput = this.add.dom(screenWidth/2, screenHeight/2 - 210).createFromCache("form");
         this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 
+        //answer detection
         //passPhrase:
         // distinctgreenpandaeatingbamboo
         this.returnKey.on("down", event => {
             let name = this.nameInput.getChildByName("inputBox");
-            if(encrypt(name.value) == "pdssdfuusbfhu"){
+            if(encrypt(name.value) == "sgfjeoitqjpnjubwjuopvc{{pxusziutbscxsfzs"){
                 this.resultText.setText("Congradulations")
             }
             else if(encrypt(name.value) === "eofbubojchnbpceptjjudohufsofbq") {
@@ -29,6 +32,10 @@ class Menu extends Phaser.Scene {
                 this.resultText.setText("Sorry, wrong answer")
             }
         });
+
+        //hint button
+        this.launchBtn = this.add.sprite(screenCenterX, screenCenterY - 120, 'launchButton').setInteractive().setScale(0.9); //Initialize the button
+        this.launchBtn.on('pointerdown', () => this.hintText.setText(randHint())); //What happens when you click   
     }
     update(){
     }
